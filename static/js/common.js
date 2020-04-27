3171238d9314c7d536023d72f9c0621d80b19425
seonghyeon <tjdgusdkf@gmail.com>
2020-04-27 19:09:25 +0900

    프론트앤드 개발 거의 마무리

--- static/js/common.js
+++ static/js/common.js
@@ -1,3 +1,18 @@
 function getAvatarImgUrl(user) {
     return `https://avatars.githubusercontent.com/${user}`;
 }
+
+window.onscroll = function() {scrollFunction()};
+    
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+	document.getElementById("topBtn").style.opacity = "1";
+  } else {
+	document.getElementById("topBtn").style.opacity = "0";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $('html, body').stop().animate( { scrollTop : 0 } ); 
+}
